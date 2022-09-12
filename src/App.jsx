3caed157f5cfd8872097eec67d053df25ea49b8e
Gar-Yin Lam <garyin.lam@gmail.com>
2022-09-12 14:03:18 +0100

    setup and boilerplate removal

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route>
+            
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
