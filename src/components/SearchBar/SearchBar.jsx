1000e4eff4502fb72a033fb34541aa9024978376
Gar-Yin Lam <garyin.lam@gmail.com>
2022-09-15 15:47:10 +0100

    sort results

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -8,18 +8,31 @@ const SearchBar = (props) => {
 
   const toggleFilter = () => setShowFilters(!showFilters);
 
-  const { getSearchTerm, getResultsNumber, getIsAcidic, getIsClassic, getAbvRange } = props
+  const { getSearchTerm, getResultsNumber, getIsAcidic, getIsClassic, getAbvRange, getSortValue } = props
   return (
     <div className='search-bar'>
-      <input type="text" onChange={getSearchTerm} placeholder="Search beers by name..." className='search-bar__textbox' />
+      <label htmlFor="beer-search">Search beers by name:</label>
+      <input id="beer-search" type="text" onChange={getSearchTerm} placeholder="Beer name..." className='search-bar__textbox' />
       <button className='search-bar__filter-toggle' onClick={toggleFilter}>Filters</button>
-      <select name="" id="" onChange={getResultsNumber} className='search-bar__results-drop' >
-        <option className='search-bar__option' value="all">All</option>
-        <option className='search-bar__option' value="160">160</option>
-        <option className='search-bar__option' value="80">80</option>
-        <option className='search-bar__option' value="40">40</option>
+      <label htmlFor="results-number">Number of results displayed:</label>
+      <select name="results-number" id="results-number" onChange={getResultsNumber} className='search-bar__results-drop' >
         <option className='search-bar__option' value="20">20</option>
+        <option className='search-bar__option' value="40">40</option>
+        <option className='search-bar__option' value="80">80</option>
+        <option className='search-bar__option' value="160">160</option>
+        <option className='search-bar__option' value="all">All</option>
       </select>
+
+      <label htmlFor="results-sort">Sort results by:</label>
+      <select name="results-sort" id="results-sort" className='search-bar__results-drop' onChange={getSortValue} >
+        <option className="search-bar__option" value="nameASC">Name (Ascending)</option>
+        <option className="search-bar__option" value="nameDSC">Name (Descending)</option>
+        <option className="search-bar__option" value="abvASC">ABV (Ascending)</option>
+        <option className="search-bar__option" value="abvDSC">ABV (Descending)</option>
+        <option className="search-bar__option" value="idASC">ID (Ascending)</option>
+        <option className="search-bar__option" value="idDSC">ID (Descending)</option>
+      </select>
+
       {showFilters&&(<FilterMenu getIsAcidic={getIsAcidic} getIsClassic={getIsClassic} getAbvRange={getAbvRange} />)}
     </div>
   )
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -8,10 +8,11 @@ import "./Home.scss"
 
 const Home = (props) => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [resultsNumber, setResultsNumber] = useState(Number.MAX_SAFE_INTEGER);
+  const [resultsNumber, setResultsNumber] = useState(20);
   const [isAcidic, setIsAcidic] = useState(false);
   const [isClassic, setIsClassic] = useState(false);
   const [abvRange, setAbvRange] = useState([0,100]);
+  const [sortValue, setSortValue] = useState("nameASC");
 
   const { beersArr } = props;
 
@@ -39,13 +40,16 @@ const Home = (props) => {
   const getAbvRange = (e) => {
     e.preventDefault();
     const min = Number(e.target[0].value);
-    const max = Number(e.target[1].value);
+    const max = Number(e.target[1].value) ? Number(e.target[1].value) : 100;
     if (min > max) {
       setAbvRange([max,min]);
     } else {
       setAbvRange([min,max]);
     }
-    console.log(abvRange);
+  }
+
+  const getSortValue = (e) => {
+    setSortValue(e.target.value);
   }
 
   const filteredBeers = 
@@ -56,6 +60,24 @@ const Home = (props) => {
     .filter((beer) => isAcidic ? beer.ph < 4 : true)
     .filter((beer) => isClassic ? Number(beer.first_brewed.slice(-2)) < 11 : true)
     .filter((beer) => beer.abv >= abvRange[0] && beer.abv <= abvRange[1])
+    .sort((a,b) => {
+      switch (sortValue) {
+        case "nameASC":
+          return a.name.localeCompare(b.name)
+        case "nameDSC":
+          return b.name.localeCompare(a.name)
+        case "abvASC":
+          return a.abv - b.abv
+        case "abvDSC":
+          return b.abv - a.abv
+        case "idASC":
+          return a.id - b.id
+        case "idDSC":
+          return b.id - a.id
+        default:
+          return 0;
+      }
+    })
     .slice(0,resultsNumber);
 
   return (
@@ -67,6 +89,7 @@ const Home = (props) => {
           getIsAcidic={getIsAcidic}
           getIsClassic={getIsClassic}
           getAbvRange={getAbvRange}
+          getSortValue={getSortValue}
         />
       </div>
       <div className='filter-menu'>
